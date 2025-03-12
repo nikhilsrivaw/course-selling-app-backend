762abc8442307c06c607cbfbfa0bb86d2e49a4ef
nikhilsrivaw <nikhilksrivastav190@gmail.com>
2025-03-12 23:25:18 +0530

    schema added

--- db.js
+++ db.js
@@ -1,31 +1,32 @@
-const {schema, default: mongoose} = require('mongoose');
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId
 
 const userSchema = schema({
-    email:{type:String , unique:true},
-    password:String,
-    firstName:String,
-    lastName:String
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String
 
 })
 const adminSchema = schema({
-    email:String,
-    password:String,
-    firstName:String,
-    lastName:String
-    
+    email: String,
+    password: String,
+    firstName: String,
+    lastName: String
+
 })
 const courseSchema = schema({
-    title:String,
-    description:String,
-    price:Number,
-    imageUrl:String,
-    
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
 
-
-    
 })
 const purchaseSchema = schema({
-    
+    userId: ObjectId,
+    courseId: ObjectId
 })
 
 
@@ -33,3 +34,11 @@ const userModel = mongoose.Model("user", userSchema);
 const adminModel = mongoose.Model("amdin", adminSchema);
 const courseModel = mongoose.Model("course", courseSchema);
 const purchaseModel = mongoose.Model("purchase", purchaseSchema);
+
+
+module.exports= {
+    userModel,
+    courseModel,
+    adminModel,
+    purchaseModel
+}
