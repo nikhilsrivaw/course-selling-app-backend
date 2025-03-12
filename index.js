0a256fedea17c42936fb501252ae774c903c1ecf
nikhilsrivaw <nikhilksrivastav190@gmail.com>
2025-03-12 19:40:53 +0530

    added

--- index.js
+++ index.js
@@ -2,17 +2,20 @@ const express = require('express');
 const { userRouter } = require('./Routes/user');
 const { courseRouter } = require('./Routes/Courses')
 
+const app = express();
+
 
 
 //Routing in express , the express Router
 app.use("/user", userRouter);
 app.use("/course", courseRouter)
 
-const app = express()
 
-createUserRoutes(app);
-createCoursesRoutes(app);
 
+
+
+
+app.listen(3000);
 
 
 
